d46123839b36df65d572714a8f76663f4d8b4f25
Li Xiangyue <113219380+huan-yin@users.noreply.github.com>
2025-10-11 15:41:11 +0800

    Update inference_sd_xl_with_pipline.py

--- inference_sd_xl_with_pipline.py
+++ inference_sd_xl_with_pipline.py
@@ -9,5 +9,5 @@ generator = torch.Generator(device).manual_seed(seed)
 vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16).to(device)
 pipeline = StableDiffusionXLPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", vae=vae, torch_dtype=torch.float16).to(device)
 pil_image = pipeline(prompt=prompt, generator=generator, num_inference_steps=20).images[0]
-pil_image.save("/data1/lxy/project/generation/FLUX/inference_test/images/sdxl_cat_with_pipline.png")
+pil_image.save("images/sdxl_cat_with_pipline.png")
 
