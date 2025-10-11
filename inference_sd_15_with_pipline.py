e91b11934cdd4c6ee1df89a90df5912025f86773
Li Xiangyue <113219380+huan-yin@users.noreply.github.com>
2025-10-11 15:40:11 +0800

    Update inference_sd_15_with_pipline.py

--- inference_sd_15_with_pipline.py
+++ inference_sd_15_with_pipline.py
@@ -7,5 +7,5 @@ seed= 42
 generator = torch.Generator(device).manual_seed(seed)
 pipeline = StableDiffusionPipeline.from_pretrained("sd-legacy/stable-diffusion-v1-5", torch_dtype=torch.float16).to(device)
 pil_image = pipeline(prompt=prompt, generator=generator).images[0]
-pil_image.save("/data1/lxy/project/generation/FLUX/inference_test/images/sd15_cat_with_pipline.png")
+pil_image.save("images/sd15_cat_with_pipline.png")
 
